4de8405867797cfbf2c820e40f49f512a6f2c1d8
Carlos G <cgarciagarcia729@gmail.com>
2026-09-13 20:31:00 -0600

    chore: add karma firefox config and update .gitignore

---
+++ karma.conf.js
@@ -0,0 +1,25 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-firefox-launcher'),
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false,
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true,
+    },
+    reporter: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['FirefoxHeadless'],
+    singleRun: true,
+    restartOnFileChange: false,
+  });
+};
